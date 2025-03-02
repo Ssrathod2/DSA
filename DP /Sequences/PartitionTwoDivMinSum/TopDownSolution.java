7b0950639dc1da8c8628640a23cbfc07bbca6771
Sandeep Rathod <rathodssm2001@gmail.com>
2025-03-02 20:43:21 +0530

    Added PerfectSumProblem

---
+++ DP /Sequences/PartitionTwoDivMinSum/TopDownSolution.java
@@ -0,0 +1,68 @@
+import java.util.*;
+
+class Solution {
+    // Function to calculate the number of subsets with a given sum
+    
+    // public int rec(int[] nums, int idx, int cursum, int target,int [][] dp){
+    //     if(cursum > target) return 0;
+    //     if(idx >= nums.length){
+    //         if(cursum == target) {
+    //             return 1;
+    //         }
+    //         else {
+    //             return 0;
+                
+    //         }
+    //     }
+    //     //cursum += nums[idx];
+    //     if(dp[idx][cursum] != -1) return dp[idx][cursum];
+
+        
+    //     int take = rec(nums,idx+1,cursum+nums[idx],target,dp);
+    //     int nottake = rec(nums,idx+1,cursum,target,dp);
+    //     dp[idx][cursum] = take+nottake;
+    //     return dp[idx][cursum];
+    //  }
+    public int perfectSum(int[] nums, int target) {
+        // code here
+        int n = nums.length;
+        int [][] dp = new int[n][target+1];
+        Arrays.stream(dp).forEach(row -> Arrays.fill(row,0));
+        //return rec(nums,0,0,target,dp);
+        
+        for(int i=0;i<n;i++){
+            dp[i][0] = 1;
+        }
+        if(nums[0] <= target) dp[0][nums[0]] += 1;
+        
+        for(int i=1;i<n;i++){
+            for(int j=0;j<=target;j++){
+                int nottake = dp[i-1][j];
+                int take = 0;
+                if(nums[i] <= j){
+                    take = dp[i-1][j-nums[i]];
+                }
+                dp[i][j] = take+nottake;
+            }
+        }
+        
+        return dp[n-1][target];
+
+       
+    }
+}
+
+/* 
+Approach :
+
+1 2 3 4 5
+
+  0 1 2 3 4 5
+0 1 0 0 0 0 0
+1 1 1 0 0 0 0
+2 
+3
+4
+5
+
+*/
---
+++ DP /Sequences/PartitionTwoDivMinSum/readme.md
@@ -0,0 +1,13 @@
+#Description :
+
+![alt text](image.png)
+
+#Solution :
+
+Top-Down Solution :
+
+![alt text](image-1.png)
+
+Bottom-up Solution :
+
+![alt text](image-2.png)
