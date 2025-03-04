e5da58fe47468c9208663fa99a3345105f60d606
Sandeep Rathod <rathodssm2001@gmail.com>
2025-03-04 09:14:36 +0530

    Added new power of 3 problem

---
+++ RandomIMPQuestions/Math/Sumof3Powers/code.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public boolean checkPowersOfThree(int n) {
+        
+        while(n>0){
+            int q = n/3;
+            int r = n%3;
+
+            if(r==2) return false;
+            n = q;
+
+        }
+        return true;
+    }
+}
+
+
---
+++ RandomIMPQuestions/Math/Sumof3Powers/readme.md
@@ -0,0 +1,11 @@
+## Description :
+
+![alt text](image.png)
+
+## Solution Explaination :
+
+![alt text](image-1.png)
+
+## Solution :
+
+![alt text](image-2.png)
